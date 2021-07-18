f54d2de3c911a88af7621e23d668fac5a0f85033
akshayca <akshay.ca.07@gmail.com>
2021-07-18 12:56:12 +0530

    Initial commit

---
+++ src/test/java/com/workspace_tdd_bdd/AppTest.java
@@ -0,0 +1,18 @@
+package com.workspace_tdd_bdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
